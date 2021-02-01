973c09c8680e79ade78659c68bf58136481223ff
Osama Yasser <osamayasserr@gmail.com>
2021-02-01 23:48:11 +0200

    Flask-Bootstrap integration

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 # Initialize the main flask application object
 app = Flask(__name__)
+
+# Initialize flask extensions
+bootstrap = Bootstrap(app)
 
 
 # Define a route that maps '/' -> index
 @app.route('/')
 def index():
-    return '<h1>Hello, World!</h1>'
+    return render_template('index.html')
 
 
 # Define a route that maps '/user/<name>' -> user
 @app.route('/user/<name>')
 def user(name):
-    return f'<h1>Hello, {name.title()}!</h1>'
+    return render_template('user.html', name=name)
