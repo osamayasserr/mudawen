6f0161ab783536d7c5d695225ef28ce4947a46e3
Osama Yasser <osamayasserr@gmail.com>
2021-05-15 07:07:14 +0200

    Performance

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or os.urandom(32)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
     MAIL_SERVER = os.getenv('MAIL_SERVER') or 'smtp.googlemail.com'
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
@@ -20,6 +21,7 @@ class Config:
     MUDAWEN_POSTS_PER_PAGE = 20
     MUDAWEN_FOLLOWERS_PER_PAGE = 50
     MUDAWEN_COMMENTS_PER_PAGE = 30
+    MUDAWEN_QUERY_TIME_LIMIT = 0.5
 
     @staticmethod
     def init_app(app):
